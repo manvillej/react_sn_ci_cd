5e9c2bf2e74a9de905f18b46c1bbddf5372267a7
Jeff Manville <jeffmanvillejr@gmail.com>
2020-01-19 14:18:47 -0500

    added main side menu, app nav bar, and data access

--- src/App.js
+++ src/App.js
@@ -1,24 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
+
+
+import { makeStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+
+import ButtonAppBar from './AppBar.js';
+
+const useStyles = makeStyles({
+  list: {
+    width: 200,
+  },
+  fullList: {
+    width: 'auto',
+  },
+});
 
 function App() {
+  const classes = useStyles();
+  const [state, setState] = React.useState({
+    left: false,
+  });
+
+  const toggleDrawer = (side, open) => event => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+
+    setState({ ...state, [side]: open });
+  };
+
+  const sideList = side => (
+    <div
+      className={classes.list}
+      role="presentation"
+      onClick={toggleDrawer(side, false)}
+      onKeyDown={toggleDrawer(side, false)}
+    >
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+
+  const [incidents, setIncidents] = useState([])
+
+  useEffect(()=>{
+     axios.get('/api/now/table/incident?sysparm_limit=10')
+          .then(res => {
+            setIncidents(res.data.result)
+          })
+  },[])
+
+  const Incidents = () => {
+    return (
+      <ul>
+        {incidents.map( (i)=>(<li key={i.sys_id}>{i.number}: {i.short_description}</li>)) }
+      </ul>
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ButtonAppBar menuButtonOnClick={toggleDrawer('left', true)}/>
+      <Incidents/>
+      <Drawer open={state.left} onClose={toggleDrawer('left', false)}>
+        {sideList('left')}
+      </Drawer>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import axios from 'axios'
+
+
+axios.defaults.headers.put["Content-Type"] = "application/json"
+
+if (process.env.NODE_ENV === 'development') {
+    const username = process.env.REACT_APP_USER
+    const password = process.env.REACT_APP_PASSWORD
+    axios.defaults.auth = {
+        username,
+        password
+    }
+} else {
+    axios.defaults.headers["X-userToken"] = window.servicenowUserToken
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
